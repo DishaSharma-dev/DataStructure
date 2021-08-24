427bc3b1aa885049eaca3a443072744adcf05a8f
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2021-08-24 12:38:43 +0530

    Update patten1.cpp

--- patten1.cpp
+++ patten1.cpp
@@ -3,6 +3,11 @@
           232
          34543          */
 
+/*Triangle of numbers
+           1
+          232
+         34543          */
+
 #include <iostream>
 using namespace std;
 
@@ -14,13 +19,13 @@ int main(){
 	for(i=1;i<=n;i++){
 		k=i;
 		for(j=1;j<2*n;j++){
-			if((j+j)>n && j<=n){
+			if((i+j)>n && j<=n){
 				cout<<k;
 				k++;
 			}
-			else if(j>=n && k>i){
+			else if((k-1)>i){
+				cout<<k-2;
 				k--;
-				cout<<k;
 			}
 			else{
 				cout<<" ";
@@ -30,4 +35,5 @@ int main(){
 	}
 	return 0;
 }
+		  
 		  
