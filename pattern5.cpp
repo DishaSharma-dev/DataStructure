da1027f0ff9a99cdd11ef55fa74d267111349dce
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2021-08-25 15:59:27 +0530

    number_pattern

---
+++ pattern5.cpp
@@ -0,0 +1,20 @@
+/*Number Pattern
+4 4 4 4 4 4 4 
+4 3 3 3 3 3 4 
+4 3 2 2 2 3 4 
+4 3 2 1 2 3 4 
+4 3 2 2 2 3 4 
+4 3 3 3 3 3 4 
+4 4 4 4 4 4 4   */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Enter the number of rows to be display: ";
+	cin>>n;
+	
+	}
+	return 0;
+}
