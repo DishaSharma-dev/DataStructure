a5970338dda8eaa8dcf1f4d923be757626c3e1ab
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2021-11-10 15:09:00 +0530

    merge sort correction

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -4,39 +4,32 @@
 using namespace std;
 
 
-void merge(int a[], int l, int m, int r){
-	int n1= m-l+1;
-	int n2= r-m;
-	int left[n1];
-	int right[n2];
-	for(int i=0;i<n1;i++)
-		left[i]=a[l+i];
-	for(int j=0;j<n2;j++)
-		right[j]=a[m+1+j];
-	int i=0;
-	int j=0;
-	int k=p;
-	while(i<n1 && j<n2){
-		if(left[i]<=right[j]){
-			a[k]=left[i];
-			i++;
-		}
-		else{
-			a[k]=right[j];
-			j++;
-		}
-	k++;
-	}
-	while(i<n1){
-		a[k]=left[i];
-		k++;
-		i++;	
-	}
-	while(j<n2){
-		a[k]=right[j];
-		k++;
-		j++;	
-	}
+void merge(int input[],int i1,int j1,int i2,int j2)
+{int temp[1000];    
+    int i,j,k;
+    i=i1;    
+    j=i2;    
+    k=0;
+    
+    while(i<=j1 && j<=j2)    
+    {
+        if(input[i]<input[j])
+            temp[k++]=input[i++];
+        else
+            temp[k++]=input[j++];
+    }
+    
+    while(i<=j1)   
+        temp[k++]=input[i++];
+        
+    while(j<=j2)    
+        temp[k++]=input[j++];
+        
+   
+    for(i=i1,j=0;i<=j2;i++,j++)
+        input[i]=temp[j];
+
+
 }
 
 void merge_sort(int a [], int l, int r){
@@ -44,7 +37,7 @@ void merge_sort(int a [], int l, int r){
 		int m=(l+r)/2;
 		merge_sort(a,l,m);
 		merge_sort(a,m+1,r);
-		merge(a,l,m,r);
+		merge(a,l,m,m+1,r);
 	}
 }
 
