d5b870f0f2488fdc933b5e67fca5755a693648db
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2022-03-06 13:32:42 +0530

    level wise input and print of binary tree

---
+++ input_print_binary_tree.cpp
@@ -0,0 +1,80 @@
+/* Input/ print in binary tree */
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+template <typename T>
+
+class BinaryTreeNode{
+	public:
+		T data;
+		BinaryTreeNode* left;
+		BinaryTreeNode* right;
+	BinaryTreeNode(T data){
+		this->data = data;
+		this->left = NULL;
+		this->right = NULL;
+	}
+};
+
+BinaryTreeNode<int>* takeInput(){
+	int data;
+	cout<<"Enter data";
+	cin>>data;
+	if(data == -1){
+		return NULL;
+	}
+	BinaryTreeNode<int>* root = new BinaryTreeNode<int>(data);
+	queue<BinaryTreeNode<int>*> q;
+	q.push(root);
+	while(q.size() != 0){
+		BinaryTreeNode<int>* front = q.front();
+		q.pop();
+		int leftChild;
+		cout<<"Enter the left child of "<<front->data<<": ";
+		cin>>leftChild;
+		if(leftChild != -1){
+			BinaryTreeNode<int>* child = new BinaryTreeNode<int>(leftChild);
+			front->left = child;
+			q.push(child);
+		}
+		int rightChild;
+		cout<<"Enter the right child of "<<front->data<<": ";
+		cin>>rightChild;
+		if(rightChild != -1){
+			BinaryTreeNode<int>* child = new BinaryTreeNode<int>(rightChild);
+			front->right = child;
+			q.push(child);
+		}
+	}
+	return root;
+}
+
+void printTree(BinaryTreeNode<int>* root){
+	if(root == NULL){
+		return;
+	}
+	queue<BinaryTreeNode<int>*> q;
+	q.push(root);
+	while(q.size() != 0){
+		BinaryTreeNode<int>* front = q.front();
+		cout<<front->data<<": ";
+		q.pop();
+		if(front->left != NULL){
+			cout<<"L: "<<front->left->data;
+			q.push(front->left);
+		}
+		if(front->right != NULL){
+			cout<<" R: "<<front->right->data;
+			q.push(front->right);
+		}
+		cout<<endl;
+	}
+}
+
+int main(){
+	BinaryTreeNode<int>* root = takeInput();
+	printTree(root);
+	return 0;
+}
