fb1d94fb8980456936d27017d93d7b7455157c50
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2022-03-07 17:03:13 +0530

    height of binary tree

---
+++ height_of_binary_tree.cpp
@@ -0,0 +1,69 @@
+/* Height of binary tree 
+Input: 10 20 30 40 50 -1 -1 -1 -1 -1 -1
+Output: 3
+*/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+template <typename T>
+
+class BinaryTreeNode{
+	public:
+		T data;
+		BinaryTreeNode* left;
+		BinaryTreeNode* right;
+	BinaryTreeNode(T data){
+		this->data = data;
+		this->left = NULL;
+		this->right = NULL;
+	}
+};
+
+BinaryTreeNode<int>* takeInput(){
+	int data;
+	cout<<"Enter data";
+	cin>>data;
+	if(data == -1){
+		return NULL;
+	}
+	BinaryTreeNode<int>* root = new BinaryTreeNode<int>(data);
+	queue<BinaryTreeNode<int>*> q;
+	q.push(root);
+	while(q.size() != 0){
+		BinaryTreeNode<int>* front = q.front();
+		q.pop();
+		int leftChild;
+		cout<<"Enter the left child of "<<front->data<<": ";
+		cin>>leftChild;
+		if(leftChild != -1){
+			BinaryTreeNode<int>* child = new BinaryTreeNode<int>(leftChild);
+			front->left = child;
+			q.push(child);
+		}
+		int rightChild;
+		cout<<"Enter the right child of "<<front->data<<": ";
+		cin>>rightChild;
+		if(rightChild != -1){
+			BinaryTreeNode<int>* child = new BinaryTreeNode<int>(rightChild);
+			front->right = child;
+			q.push(child);
+		}
+	}
+	return root;
+}
+
+int height(BinaryTreeNode<int>* root){
+	if(root == NULL){
+		return 0;
+	}
+	return max(height(root->left), height(root->right)) + 1;
+}
+
+int main(){
+	BinaryTreeNode<int>* root = takeInput();
+	cout<<"Height of tree is: ";
+	cout<<height(root);
+	return 0;
+}
