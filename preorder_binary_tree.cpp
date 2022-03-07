33a410838fc0309fa7bcca921502e6586c8fc186
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2022-03-07 23:36:45 +0530

    preorder of binary tree

---
+++ preorder_binary_tree.cpp
@@ -0,0 +1,67 @@
+/* Preorder Binary Tree */
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+template <typename T>
+
+class BinaryTreeNode{
+	public:
+		T data;
+		BinaryTreeNode* left;
+		BinaryTreeNode* right;
+	BinaryTreeNode(T data){
+		this->data = data;
+		this->left = NULL;
+		this->right = NULL;
+	}
+};
+
+BinaryTreeNode<int>* takeInput(){
+	int data;
+	cout<<"Enter data";
+	cin>>data;
+	if(data == -1){
+		return NULL;
+	}
+	BinaryTreeNode<int>* root = new BinaryTreeNode<int>(data);
+	queue<BinaryTreeNode<int>*> q;
+	q.push(root);
+	while(q.size() != 0){
+		BinaryTreeNode<int>* front = q.front();
+		q.pop();
+		int leftChild;
+		cout<<"Enter the left child of "<<front->data<<": ";
+		cin>>leftChild;
+		if(leftChild != -1){
+			BinaryTreeNode<int>* child = new BinaryTreeNode<int>(leftChild);
+			front->left = child;
+			q.push(child);
+		}
+		int rightChild;
+		cout<<"Enter the right child of "<<front->data<<": ";
+		cin>>rightChild;
+		if(rightChild != -1){
+			BinaryTreeNode<int>* child = new BinaryTreeNode<int>(rightChild);
+			front->right = child;
+			q.push(child);
+		}
+	}
+	return root;
+}
+
+void preorder(BinaryTreeNode<int>* root){
+	if(root == NULL){
+		return;
+	}
+	cout<<root->data<<" ";
+	preorder(root->left);
+	preorder(root->right);
+}
+
+int main(){
+	BinaryTreeNode<int>* root = takeInput();
+	preorder(root);
+	return 0;
+}
